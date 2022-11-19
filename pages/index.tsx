793c89e8232c5ff49474001a0d198b1b5be554dd
Naryond <giuggey85@gmail.com>
2022-11-19 16:25:22 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function Home() {
+  const [content, setContent] = useState<string>('');
+  const [author, setAuthor] = useState<string>('');
+
+  const fetching = async (): Promise<void> => {
+    let result = await fetch('https://api.quotable.io/random');
+    if (result.ok) {
+      let response = await result.json();
+      setContent(response.content);
+      setAuthor(response.author);
+    }
+  };
+  return (
+    <div>
+      <h1 className="title">Quote of the Day</h1>
+      <button className="button" onClick={fetching}>
+        Obtain
+      </button>
+      <div className="scroll">{content}</div>
+      <div className="author">{author}</div>
+    </div>
+  );
+}
+
+export default Home;
