3a51f1b4f69e7335fc8ca9577b3ee82f29481661
Naryond <giuggey85@gmail.com>
2022-12-13 22:25:46 +0000

    Title page set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app';
 import '../styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
+
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Random Quotes Generator</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
